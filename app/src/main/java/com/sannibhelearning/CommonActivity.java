6b507075aa7a109737cfa1e5f6f17693ff30db37
Lav <hinsulav105@gmail.com>
2019-06-10 17:52:39 +0530

    Converted Tab view To Tab Host

--- app/src/main/java/com/sannibhelearning/CommonActivity.java
+++ app/src/main/java/com/sannibhelearning/CommonActivity.java
@@ -1,7 +1,7 @@
 package com.sannibhelearning;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class CommonActivity extends AppCompatActivity {
 
@@ -9,6 +9,7 @@ public class CommonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common);
-        
+
+
     }
 }
