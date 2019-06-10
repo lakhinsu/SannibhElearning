7fa0cf2944ef1c9cedeaeb3097965282df663ade
Lav <hinsulav105@gmail.com>
2019-06-10 18:33:18 +0530

    Test Commit

--- app/src/main/java/com/sannibhelearning/CommonActivity.java
+++ app/src/main/java/com/sannibhelearning/CommonActivity.java
@@ -2,8 +2,12 @@ package com.sannibhelearning;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 public class CommonActivity extends AppCompatActivity {
+    private TabHost tabHost;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
