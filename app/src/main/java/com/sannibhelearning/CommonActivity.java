1c486b418b9081a1dd311dcccdcecbc61e9fb399
KrushitSheth <51577578+KrushitSheth@users.noreply.github.com>
2019-06-11 11:44:45 +0530

    Added Tab view

--- app/src/main/java/com/sannibhelearning/CommonActivity.java
+++ app/src/main/java/com/sannibhelearning/CommonActivity.java
@@ -1,5 +1,7 @@
 package com.sannibhelearning;
 
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
@@ -13,7 +15,13 @@ public class CommonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common);
-
-
+        TabHost.TabSpec mycoursespec=tabHost.newTabSpec("mycourse");
+        Intent mycourseintent=new Intent(this,MyCourses.class);
+        mycoursespec.setContent(mycourseintent);
+        TabHost.TabSpec allcoursespec=tabHost.newTabSpec("allcourse");
+        Intent allcourseintent=new Intent(this,AllCourses.class);
+        allcoursespec.setContent(mycourseintent);
+        tabHost.addTab(mycoursespec);
+        tabHost.addTab(allcoursespec);
     }
 }
