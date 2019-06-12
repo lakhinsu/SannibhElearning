fca52ae922ae02495f68d67a3b9feda7b26ef41e
Lav <hinsulav105@gmail.com>
2019-06-12 11:12:15 +0530

    Added ListView To Tab

--- app/src/main/java/com/sannibhelearning/Tab1.java
+++ app/src/main/java/com/sannibhelearning/Tab1.java
@@ -2,19 +2,23 @@ package com.sannibhelearning;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class Tab1 extends Fragment {
+/*public class Tab1 extends Fragment {
     String[] values = new String[]{"India", "java", "c++", "Ad.Java", "Linux", "Unix"};
     ListView lv;
 
     //Overriden method onCreateView
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        
+
 
         //Returning the layout file after inflating
         //Change R.layout.tab1 in you classes
@@ -22,4 +26,29 @@ public class Tab1 extends Fragment {
 
         return inflater.inflate(R.layout.tab1, container, false);
     }
+}*/
+public class Tab1 extends ListFragment implements AdapterView.OnItemClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab1, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Planets, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
