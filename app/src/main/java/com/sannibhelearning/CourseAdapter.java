06db977504a023617904d259d371569ed5205363
Lav <hinsulav105@gmail.com>
2019-06-15 12:32:05 +0530

    Added cards for all courses.

---
+++ app/src/main/java/com/sannibhelearning/CourseAdapter.java
@@ -0,0 +1,79 @@
+package com.sannibhelearning;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.MyViewHolder> {
+
+
+    private Context mContext;
+    private List<CourseModel> courseslist;
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, description;
+        public ImageView thumbnail;
+        public Button overflow;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            description= (TextView) view.findViewById(R.id.description);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            overflow = (Button) view.findViewById(R.id.overflow);
+        }
+    }
+
+    public CourseAdapter(Context mContext, List<CourseModel> albumList) {
+        this.mContext = mContext;
+        this.courseslist = albumList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.courses_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        final CourseModel courseModel = courseslist.get(position);
+        holder.title.setText(courseModel.getCouresname());
+        holder.description.setText(courseModel.getDescription());
+
+        // loading album cover using Glide library
+
+        Picasso.get().load("http://192.168.0.4:80/sannibh/admin/"+courseModel.getUrl()).into(holder.thumbnail);
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext,"clicked!"+courseModel.getCouresname(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return courseslist.size();
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/com/sannibhelearning/Tab2.java
+++ app/src/main/java/com/sannibhelearning/Tab2.java
@@ -1,7 +1,13 @@
 package com.sannibhelearning;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +16,15 @@ import android.widget.BaseExpandableListAdapter;
 import com.diegocarloslima.fgelv.lib.FloatingGroupExpandableListView;
 import com.diegocarloslima.fgelv.lib.WrapperExpandableListAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tab2 extends Fragment {
+
+    RecyclerView recyclerView;
+
+    List<CourseModel> courseModels;
+    CourseAdapter courseAdapter;
 
     //Overriden method onCreateView
     @Override
@@ -20,6 +34,88 @@ public class Tab2 extends Fragment {
 //Change R.layout.tab2 in you classes
 
 
-        return inflater.inflate(R.layout.tab2, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+
+        courseModels= new ArrayList<>();
+
+
+        courseAdapter=new CourseAdapter(getContext(),courseModels);
+
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getContext(), 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(1, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(courseAdapter);
+
+        prepareAlbums();
+
+
+        return view;
+    }
+
+    private void prepareAlbums() {
+
+        CourseModel course1=new CourseModel("26","Python","Learn Python the hard way","/courses/Python/image/Python.jpg");
+        courseModels.add(course1);
+
+        CourseModel course2=new CourseModel("26","Python","Learn Python the hard way","/courses/Python/image/Python.jpg");
+        courseModels.add(course2);
+
+        CourseModel course3=new CourseModel("26","Python","Learn Python the hard way","/courses/Python/image/Python.jpg");
+        courseModels.add(course3);
+
+
+
+
+
+        courseAdapter.notifyDataSetChanged();
+    }
+
+
+
+
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
